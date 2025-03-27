52e5e03229f2bf1b5c142751e8b84e33243de4b2
prj <1957457389@qq.com>
2025-03-27 13:50:31 +0800

    feat:鼠标动画

--- src/components/home/MainIcon.vue
+++ src/components/home/MainIcon.vue
@@ -170,7 +170,7 @@ const selectIcon = (icon) => {
 }
 .box:hover {
   transform: translateY(-10px); /* 图标往上移动 */
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  /* box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); */
 }
 .img-box {
   margin: 20px;
