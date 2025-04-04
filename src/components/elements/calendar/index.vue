fe1396b90f9fb361eaf3344f480885021f5a6124
prj <1957457389@qq.com>
2025-04-04 23:04:02 +0800

    style:修改样式布局

--- src/components/elements/calendar/index.vue
+++ src/components/elements/calendar/index.vue
@@ -4,8 +4,8 @@
     <!-- 1x1 布局内容 -->
     <div v-if="size === 1" class="content-1x1">
       <div class="flex flex-direction justify-around align-center">
-        <div class="oneBackground">
-          <div class="oneWeek">{{ dayOfWeekText }}</div>
+        <div class="background-1x1">
+          <div class="week-1x1">{{ dayOfWeekText }}</div>
           <div class="day">{{ date }}</div>
         </div>
         <p class="cl-ant-p sg-omit-sm text-white-sm">日历</p>
@@ -14,11 +14,11 @@
 
     <!-- 2x2 布局内容 -->
     <div v-else-if="size === '2x2'" class="content-2x2">
-      <div class="header">{{ month }}</div>
-      <div class="body">
-        <div class="num">{{ date }}</div>
+      <div class="header-2x2">{{ month }}</div>
+      <div class="body-2x2">
+        <div class="num-2x2">{{ date }}</div>
         <div class="day">第205天 第30周</div>
-        <div class="week">六月二十八 {{ dayOfWeekText }}</div>
+        <div class="week-2x2">六月二十八 {{ dayOfWeekText }}</div>
       </div>
     </div>
   </div>
@@ -89,31 +89,33 @@ const month = ref(today.format("YYYY年M月"));
   align-items: center;
 }
 
-.oneBackground {
-  text-align: center;
+.background-1x1 {
+  width: 60px;
+  height: 60px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
-.oneWeek {
-  font-size: 12px;
+.week-1x1 {
+  font-size: 14px;
+  color: #d83030;
 }
 
-.calendar-label {
-  font-size: 12px;
-  color: #666;
-}
-
-/* end------- */
+/* 1x1 内容布局end------- */
 
 /* 2x2 内容布局 */
 .content-2x2 {
   width: 100%;
   height: 100%;
   display: flex;
- 
+
   flex-direction: column;
 }
 
-.header {
+.header-2x2 {
   /* height: 40px; */
   background-color: #ff5a5d;
   color: white;
@@ -126,7 +128,7 @@ const month = ref(today.format("YYYY年M月"));
   overflow: hidden;
 }
 
-.body {
+.body-2x2 {
   padding: 10px;
   display: flex;
   flex-direction: column;
@@ -134,19 +136,23 @@ const month = ref(today.format("YYYY年M月"));
   justify-content: center;
 }
 
-.num {
+.num-2x2 {
   font-size: 40px;
   font-weight: bold;
 }
 
-.day {
-  font-size: 12px;
-  color: gray;
-}
-
-.week {
+.week-2x2 {
   font-size: 14px;
 }
 
 /* end--- */
+
+/* 公共样式 */
+
+.day {
+  color: gray;
+  font-size: 14px;
+}
+
+/* 公共样式end */
 </style>
